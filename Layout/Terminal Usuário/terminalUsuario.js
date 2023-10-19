50497a2524d8ab3da4dd0aeb613eff30208ac8c1
jukimura <juliazinha.kimura@gmail.com>
2023-10-19 20:03:50 -0300

    First version - TerminalUsuario

---
+++ Layout/Terminal Usuário/terminalUsuario.js
@@ -0,0 +1,20 @@
+
+function exibirPaginaCorreta(pagina) {
+    // Ocultar todas as páginas
+    var pages = document.getElementsByClassName('conteudo');
+    for (var i = 0; i < pages.length; i++) {
+      pages[i].classList.remove('visible');
+    }
+  
+    // Exibir a página desejada
+    var exibir = document.getElementById(pagina);
+    if (exibir) {
+      exibir.classList.add('visible');
+    }
+  }
+  
+
+  function showDetails() {
+    alert("O serviço foi comprado");
+    // Você pode adicionar mais lógica aqui para exibir detalhes ou realizar outras ações.
+}
